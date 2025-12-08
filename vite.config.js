b3f7c9ffcdd5b3b60660bbeec3717e1228c92c59
Ravneet Kaur <ravneetkaur@Ravneets-MacBook-Air.local>
2025-12-08 16:18:33 +0530

    removed base from vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
